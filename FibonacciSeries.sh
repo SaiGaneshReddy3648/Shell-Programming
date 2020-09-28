cc2b38d30943242cb238ce75af190dafd1578109
Sai Ganesh Reddy Buthukuri <52876614+SaiGaneshReddy3648@users.noreply.github.com>
2020-09-28 10:12:21 +0530

    Update FibonacciSeries.sh

--- FibonacciSeries.sh
+++ FibonacciSeries.sh
@@ -1,17 +1,17 @@
 #Write a Shell Programming for Fibonacci Series
-echo"How many number of terms to be generated?"
+echo "How many number of terms to be generated?"
 read n
 x=0
 y=1
 i=2
-echo"Fibonacci Series upto $n terms:"
+echo "Fibonacci Series upto $n terms:"
 echo "$x"
 echo "$y"
 while[$i -lt $n]
 do
-i='expr $i+1'
-z='expr $x+$y'
-echo "$z"
-x=$y
-y=$z
+    i='expr $i+1'
+    z='expr $x+$y'
+    echo "$z"
+    x=$y
+    y=$z
 done
