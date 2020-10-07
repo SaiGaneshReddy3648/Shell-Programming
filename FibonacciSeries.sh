9b341dc37566617f9a51a9ffa03aaecc55f48e9b
Sai Ganesh Reddy Buthukuri <52876614+SaiGaneshReddy3648@users.noreply.github.com>
2020-10-07 17:10:45 +0530

    Update FibonacciSeries.sh

--- FibonacciSeries.sh
+++ FibonacciSeries.sh
@@ -1,18 +1,16 @@
 #Write a Shell Programming for Fibonacci Series
 #!/bin/sh
-echo "How many number of terms to be generated?"
-read n
-x=0
-y=1
-i=2
-echo "Fibonacci Series upto $n terms:"
-echo "$x"
-echo "$y"
-while[$i -lt $n]
-do
-    i='expr $i+1'
-    z='expr $x+$y'
-    echo "$z"
-    x=$y
-    y=$z
-done
+echo "this is the small caculator"
+echo "eneter two numbers"
+read a
+read b
+echo "1.Addition 2.Subtraction 3.Multiplication 4.Division"
+echo "enter your choice"
+read ch
+case $ch in
+	1)echo "the Addition of two numbers is:$res=$(($a+$b))";;
+	2)echo "the Subtraction of two numbers is:$res=$(($a-$b))";;
+	3)echo "the Multiplication of two numbers is:$res=$(($a*$b))";;
+	4)echo "the Division of two numbers is:$res=$(($a/$b))";;
+	*)echo "wrong option..."
+esac
